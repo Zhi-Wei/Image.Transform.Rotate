a982caa248a1eec4160ad2db6bc5f7055a31f0ac
Zhi-Wei <ZhiWei.Lu@outlook.com>
2016-12-07 19:37:05 +0800

    加入專案檔案。

---
+++ Image.Transform.Rotate/Services/Interfaces/IImageTransformService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image.Transform.Rotate.Services.Interfaces
+{
+    /// <summary>
+    /// 定義影像轉換服務方法。
+    /// </summary>
+    public interface IImageTransformService
+    {
+        /// <summary>
+        /// 旋轉影像。
+        /// </summary>
+        /// <param name="degrees">旋轉角度，–360 至 360 度。</param>
+        /// <returns>旋轉後的影像。</returns>
+        Bitmap RotateImage(double degrees);
+    }
+}
