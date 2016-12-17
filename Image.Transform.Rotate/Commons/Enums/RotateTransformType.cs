5c9df9ef4dc1f6c5d64cdc82f2183aee9992799e
Zhi-Wei <ZhiWei.Lu@outlook.com>
2016-12-17 10:40:42 +0800

    新建 RotateTransformType 影像旋轉類型列舉。

---
+++ Image.Transform.Rotate/Commons/Enums/RotateTransformType.cs
@@ -0,0 +1,18 @@
+﻿namespace Image.Transform.Rotate.Commons.Enums
+{
+    /// <summary>
+    /// 影像旋轉類型。
+    /// </summary>
+    public enum RotateTransformType
+    {
+        /// <summary>
+        /// 自訂影像旋轉。
+        /// </summary>
+        Custom = 0,
+
+        /// <summary>
+        /// GDI+ 影像旋轉。
+        /// </summary>
+        GdiPlus = 1,
+    }
+}
