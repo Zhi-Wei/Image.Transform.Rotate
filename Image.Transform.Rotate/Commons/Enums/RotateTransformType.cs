5943916e96094b31d33ead6a8b6e66258bdc302a
Zhi-Wei <ZhiWei.Lu@outlook.com>
2016-12-17 13:52:46 +0800

    新增 RotateTransformType 欄位的 DisplayAttribute。

--- Image.Transform.Rotate/Commons/Enums/RotateTransformType.cs
+++ Image.Transform.Rotate/Commons/Enums/RotateTransformType.cs
@@ -1,4 +1,6 @@
-﻿namespace Image.Transform.Rotate.Commons.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Image.Transform.Rotate.Commons.Enums
 {
     /// <summary>
     /// 影像旋轉類型。
@@ -8,11 +10,13 @@
         /// <summary>
         /// 自訂影像旋轉。
         /// </summary>
+        [Display(Name = "自訂")]
         Custom = 0,
 
         /// <summary>
         /// GDI+ 影像旋轉。
         /// </summary>
+        [Display(Name = "GDI+")]
         GdiPlus = 1,
     }
 }
