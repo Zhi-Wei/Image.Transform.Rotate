3d9f05e6d6c70c6a5da0407f4ecad39b2d0cef6f
Zhi-Wei <ZhiWei.Lu@outlook.com>
2016-12-17 12:22:29 +0800

    建立 ImageTransformStrategyFactory 影像轉換策略工廠類別。

---
+++ Image.Transform.Rotate/Factories/ImageTransformStrategyFactory.cs
@@ -0,0 +1,29 @@
+﻿using Image.Transform.Rotate.Commons.Enums;
+using Image.Transform.Rotate.Services;
+using Image.Transform.Rotate.Services.Interfaces;
+
+namespace Image.Transform.Rotate.Factories
+{
+    /// <summary>
+    /// 影像轉換策略工廠。
+    /// </summary>
+    public class ImageTransformStrategyFactory
+    {
+        /// <summary>
+        /// 取得影像轉換策略的服務實體。
+        /// </summary>
+        /// <param name="type">影像旋轉類型。</param>
+        /// <returns>影像轉換服務實體。</returns>
+        public static IImageTransformService GetStrategy(RotateTransformType type)
+        {
+            IImageTransformService service = new ImageTransformService();
+            switch (type)
+            {
+                case RotateTransformType.GdiPlus:
+                    service = new GdiPlusTransformService();
+                    break;
+            }
+            return service;
+        }
+    }
+}
